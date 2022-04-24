874f1fcf7dfcdbef525b52af9c05632d2eadd836
Julian Henao DMS <julian.henao@dms.ms>
2022-04-24 14:14:16 -0500

    Define sidenav bar con input y botón para busqueda, lista de placas y top bar con logo y botones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,54 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// Material
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatListModule } from '@angular/material/list';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatNativeDateModule } from '@angular/material/core';
+import { SidenavComponent } from './templates/sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatListModule,
+    MatTooltipModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/templates/sidenav/sidenav.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.scss']
+})
+export class SidenavComponent {
+  value = 'PEFGHS20858';
+  @ViewChild('drawer') drawer: any; 
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
+ 
+   constructor(private observer: BreakpointObserver) {}
+ 
+   ngAfterViewInit() {
+     this.observer.observe(['(max-width: 800px)']).subscribe((res) => {
+       if (res.matches) {
+         this.sidenav.mode = 'over';
+         this.sidenav.close();
+       } else {
+         this.sidenav.mode = 'side';
+         this.sidenav.open();
+       }
+     });
+   }
+
+   closeSideNav() {
+    if (this.drawer._mode=='over') {
+      this.drawer.close();
+    }
+  }
+ }
